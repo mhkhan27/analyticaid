3d5b64b4bb0f619aeeb4178175fabb10c8ce9c6c
Md Mehedi Hasan KHAN <plannermehedi@gamil.com>
2023-02-06 13:00:48 +0300

    adding data

---
+++ data preparation.R
@@ -0,0 +1,7 @@
+# load("data/data.rda")
+#
+# list2env(data,.GlobalEnv)
+#
+# list(ls())
+#
+# save(analysis_by_group,file = paste0("data/","analysis_by_group",".rda"))
---
+++ tests/testthat/test-Fix_data_type.R
@@ -0,0 +1,17 @@
+## test for find_qualitative_cols_from_kobo
+
+
+## test for number only
+
+
+## test find_interger_cols
+
+
+## test for fix_data_type (with character)
+
+
+
+## test for fix_data_type (without character)
+
+
+## test for fix_data_type (with remove_all_NA_col ==F)
